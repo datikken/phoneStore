d8a455b51e37be85bef2c87d9474c067ed6c210d
Никита Шадыбеков <tikken@MacBook-Air-Nikita.local>
2019-05-19 21:54:01 +0300

    404page

--- src/components/Default.jsx
+++ src/components/Default.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 export default class Default extends Component {
+  
   render() {
+  console.log(this.props);
+
     return (
-      <div>
-        <h3>Page not found</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="display-3">404</h1>
+            <h1>error</h1>
+            <h3>page not found</h3>
+            <h3>
+            the requested <span className="text-danger">{this.props.location.pathname}</span> doesn't exists
+            </h3>
+          </div>
+        </div>
       </div>
     )
   }
