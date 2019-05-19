af88afcef78ec6a93c55b6cd8aef28ed0025f520
Никита Шадыбеков <tikken@MacBook-Air-Nikita.local>
2019-05-19 21:48:53 +0300

    cart

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import {ProductConsumer} from '../context';
+import PropTypes from 'prop-types';
 
 export default class Product extends Component {
   render() {
@@ -10,17 +11,22 @@ export default class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
           <div className="card">
-            <div className="img-container p-5" onClick={() => console.log('clicked')
-            }>
-              <Link to="/details">
-                <img src={img} alt="product" className="card-img-top"/>
-              </Link>
-              <button className="cart-btn" disabled={inCart ? true : false} onClick={()=> {
-                console.log('added to the cart')
-              }}>
-              {inCart ? (<p className="text-capitalize mb-0" disabled>in inCart</p>) : (<i className="fas fa-cart-plus"></i>)}
-              </button>
-            </div>
+         <ProductConsumer>
+           {(value) => (
+              <div className="img-container p-5" onClick={() => value.handleDetail(id)
+              }>
+                <Link to="/details">
+                  <img src={img} alt="product" className="card-img-top"/>
+                </Link>
+                <button className="cart-btn" disabled={inCart ? true : false} onClick={()=> {
+                  value.addToCart(id);
+                  value.openModal(id);
+                }}>
+                {inCart ? (<p className="text-capitalize mb-0" disabled>in inCart</p>) : (<i className="fas fa-cart-plus"></i>)}
+                </button>
+              </div>
+           )}
+         </ProductConsumer>
             <div className="card-footer d-flex justify-content-between">
               <p className="align-self-center mb-0">
                 {title}
@@ -35,7 +41,15 @@ export default class Product extends Component {
     )
   }
 }
-
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    inCart: PropTypes.bool
+  }).isRequired
+};
 const ProductWrapper = styled.div`
   .card {
     border-color: transparent;
@@ -82,5 +96,6 @@ const ProductWrapper = styled.div`
   .cart-btn:hover {
     color: var(--mainBlue);
     cursor: pointer;
+    outline: none;
   }
 `;
